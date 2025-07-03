2ab630cc4ce310b3eab89d423cd7813b5afe2251
Cimahpar Stoneyard <infocsybogor@gmail.com>
2025-07-03 10:03:22 +0000

    Update photo untuk TPQ Al Mumtaz dan berita duka dengan public/images

--- next.config.ts
+++ next.config.ts
@@ -16,12 +16,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      },
     ],
   },
 };
